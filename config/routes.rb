a39803e0c0f4fc51507369f2a5f8abc422318fa3
rennanc <rennanchagas@outlook.com>
2018-12-08 22:01:29 +0000

    fix users with messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :minions
   resources :bookings
-  get 'static_pages/home'
-  get  '/help',    to: 'static_pages#help'
   resources :microposts
   resources :users
+  get 'static_pages/home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
